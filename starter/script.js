251c760dc6d560a3d4a7b51b1b22e39f76c05190
TraceyPen <traceylpender@yahoo.co.uk>
2024-01-06 23:33:18 +0000

    password length validation

--- starter/script.js
+++ starter/script.js
@@ -88,10 +88,44 @@ var upperCasedCharacters = [
   'Z'
 ];
 
-// Function to prompt user for password options
-function getPasswordOptions() {
+var numOfCharacters
+var includeSpecial
+var includeNumeric
+var includeLower
+var includeUpper
+var allNumbers = [0,1,2,3,4,5,6,7,8,9]
 
+// Function to prompt user for password options
+getPasswordOptions()
+
+function getPasswordOptions() {
+numOfCharacters = prompt("How many characters would you like your password to contain?");
+
+while (numOfCharacters < 8 || numOfCharacters > 128 ) {
+  
+  if(numOfCharacters<8){
+     alert("Your password must contain at least 8 characters")
+     getPasswordOptions()
+    }
+    else if(numOfCharacters>128){
+     alert ("Your password cannot contain more than 128 characters")
+     getPasswordOptions()
+    }
+  }
 }
+
+//if number less than 5 alert - "Password length must be at least 8 characters" (no more than 128!)
+//alert if empty
+includeSpecial = confirm("Click OK to include special characters")
+includeNumeric = confirm("Click OK to include numeric characters")
+includeLower = confirm("Click OK to include lowercase characters")
+includeUpper = confirm("Click OK to include uppercase characters")
+//alert if user does not choose any characters
+
+//}
+//call the getPasswordOptions() function for testing
+getPasswordOptions()
+
 
 // Function for getting a random element from an array
 function getRandom(arr) {
